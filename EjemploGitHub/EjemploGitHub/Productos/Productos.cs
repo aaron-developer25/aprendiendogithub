d67dff7e7578a5c2fdaf47d0247ed34f9317ae06
aaron-developer25 <atminifg655@gmail.com>
2024-02-01 18:21:04 -0400

    Hemos agregado la clase productos y agregado las 2 funciones requeridas.

---
+++ EjemploGitHub/EjemploGitHub/Productos/Productos.cs
@@ -0,0 +1,17 @@
+﻿namespace EjemploGitHub.Productos
+{
+    public class Productos
+    {
+
+        public void agregarProd()
+        {
+
+        }
+
+        public void ëditarProd()
+        {
+
+        }
+
+    }
+}
